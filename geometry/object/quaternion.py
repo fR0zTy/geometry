d843b4e3863c86ffe429c0af058a342af44585f6
darshn <darshan.patel@arculus.de>
2020-05-20 15:38:35 +0200

    add basic arithmetic for quaternion

--- geometry/object/quaternion.py
+++ geometry/object/quaternion.py
@@ -1,12 +1,13 @@
 # -*- coding : utf-8 -*-
-from math import sqrt, isclose
+from math import sqrt, isclose, acos, cos, sin
 from typing import Generator, Tuple
 
 from geometry.types import Real
 from geometry import Vector
+from geometry.utilities.copyable import CopyableMixin
 
 
-class Quaternion:
+class Quaternion(CopyableMixin):
 
     def __init__(self, w: Real = 1, x: Real = 0, y: Real = 0, z: Real = 0):
         self.scalar = w
@@ -69,10 +70,42 @@ class Quaternion:
 
     def __mul__(self, other: "Quaternion") -> "Quaternion":
         if not isinstance(other, Quaternion):
-            raise TypeError(f"Cannot multiply instances of type {type(other)} and {type(self)}")
+            other = Quaternion.from_scalar(other)
+
         _scalar = self.scalar * other.scalar - self.vector.dot(other.vector)
         _vector = other.vector.scale(self.scalar) + self.vector.scale(other.scalar) + self.vector.cross(other.vector)
         return Quaternion(_scalar, *_vector)
+
+    def __imul__(self, other: "Quaternion") -> "Quaternion":
+        return self * other
+
+    def __rmul__(self, other: "Quaternion") -> "Quaternion":
+        if not isinstance(other, Quaternion):
+            other = Quaternion.from_scalar(other)
+        return other * self
+
+    def __div__(self, other: "Quaternion") -> "Quaternion":
+        if not isinstance(other, Quaternion):
+            other = Quaternion.from_scalar(other)
+        if other.is_zero_quaternion():
+            raise ZeroDivisionError("other is a zero quaternion!")
+        return other * self.inverse()
+
+    def __truediv__(self, other: "Quaternion") -> "Quaternion":
+        return self.__div__(other)
+
+    def __pow__(self, exponent: Real) -> "Quaternion":
+        norm = self.norm()
+        if norm > 0:
+            vector_mag = self.vector.magnitude()
+            if vector_mag <= 0:
+                return Quaternion.from_scalar(self.scalar ** exponent)
+            unit_vector = self.vector.scale(1 / vector_mag)
+            phi = acos(self.scalar / norm)
+            _scalar = cos(exponent * phi)
+            _vector = unit_vector.scale(sin(exponent * phi))
+            return (norm ** exponent) * Quaternion(_scalar, *_vector)
+        return self.copy()
 
     def __eq__(self, other: "Quaternion") -> "Quaternion":
         return all(isclose(i, j, rel_tol=1e-09, abs_tol=1e-09) for i, j in zip(self, other))
@@ -80,14 +113,35 @@ class Quaternion:
     def __hash__(self) -> int:
         return hash(self.as_tuple())
 
-    def norm(self) -> "Quaternion":
-        return sqrt(self.scalar ** 2 + self.vector.dot(self.vector))
+    def is_zero_quaternion(self) -> bool:
+        return self == Quaternion(0, 0, 0, 0)
+
+    def is_unit_quaternion(self) -> bool:
+        return isclose(self._squared_sum(), 1.0, rel_tol=1e-09)
+
+    def magnitude(self) -> Real:
+        return self.norm()
+
+    def _squared_sum(self) -> Real:
+        return self.scalar ** 2 + self.vector.dot(self.vector)
+
+    def norm(self) -> Real:
+        return sqrt(self._squared_sum())
+
+    def normalize(self) -> "Quaternion":
+        if self.is_zero_quaternion():
+            raise ValueError("Cannot normalize a zero quaternion!")
+        n = self.norm()
+        return Quaternion(*(i / n for i in self))
 
     def inverse(self) -> "Quaternion":
-        pass
+        if self.is_zero_quaternion():
+            raise ValueError("Cannot invert a zero quaternion!")
+        square_sum = self._squared_sum()
+        return Quaternion(self.scalar / square_sum, *-self.vector.scale(1 / square_sum))
 
     def conjugate(self) -> "Quaternion":
-        return Quaternion(self.scalar, self.vector.scale(-1))
+        return Quaternion(self.scalar, -self.vector)
 
     def as_tuple(self) -> Tuple[Real]:
         return tuple(i for i in self)
@@ -99,4 +153,20 @@ class Quaternion:
 
     @classmethod
     def identity(cls) -> "Quaternion":
-        return cls()
+        return cls(1, 0, 0, 0)
+
+    @classmethod
+    def i(cls) -> "Quaternion":
+        return cls(0, 1, 0, 0)
+
+    @classmethod
+    def j(cls) -> "Quaternion":
+        return cls(0, 0, 1, 0)
+
+    @classmethod
+    def k(cls) -> "Quaternion":
+        return cls(0, 0, 0, 1)
+
+    @classmethod
+    def from_scalar(cls, scalar: Real) -> "Quaternion":
+        return cls(scalar, 0, 0, 0)
--- geometry/object/test/test_quaternion.py
+++ geometry/object/test/test_quaternion.py
@@ -1,5 +1,7 @@
 # -*- coding : utf-8 -*-
 
+import pytest
+
 from geometry import Quaternion
 
 
@@ -18,7 +20,95 @@ def test_quaternion_add():
     q3 = Quaternion(2, 4, 6, 8)
     assert q1 + q2 == q3
 
+    q1 += q2
+    assert q1 == q3
+
+    with pytest.raises(TypeError):
+        q1 + 45
+
 
 def test_quaternion_neg():
     q1 = Quaternion(1, 2, 3, 4)
     assert -q1 == Quaternion(-1, -2, -3, -4)
+
+
+def test_quaternion_sub():
+    q1 = Quaternion(1, 2, 3, 4)
+    q2 = Quaternion(1, 1, 0, 0)
+    q3 = Quaternion(0, 1, 3, 4)
+
+    assert q1 - q2 == q3
+
+    q1 -= q2
+    assert q1 == q3
+
+    with pytest.raises(TypeError):
+        q1 - 45
+
+
+def test_quaternion_mul_basis():
+    _ = Quaternion.identity()
+    i = Quaternion.i()
+    j = Quaternion.j()
+    k = Quaternion.k()
+
+    # test identity multiplication
+    assert _ * i == i
+    assert i * _ == i
+    assert j * _ == j
+    assert _ * j == j
+    assert _ * k == k
+    assert k * _ == k
+
+    i2 = i * i
+    j2 = j * j
+    k2 = k * k
+    ijk = i * j * k
+    assert i2 == j2 == k2 == ijk == -_
+
+
+def test_quaternion_mul_real():
+    q1 = Quaternion(1, 2, 3, 4)
+    real = 4
+    q2 = Quaternion(*(i * real for i in q1))
+
+    assert q1 * real == q2
+
+
+def test_quaternion_div():
+    q1 = Quaternion(1, 2, 3, 4)
+
+    assert q1 / q1 == Quaternion.identity()
+
+
+def test_quaternion_pow():
+    _ = Quaternion.identity()
+    i = Quaternion.i()
+    j = Quaternion.j()
+    k = Quaternion.k()
+
+    i2 = i ** 2
+    j2 = j ** 2
+    k2 = k ** 2
+
+    assert i2 == j2 == k2
+
+
+def test_quaternion_normalize():
+    q1 = Quaternion(1, 2, 3, 4)
+    normalized = q1.normalize()
+    assert normalized.is_unit_quaternion()
+
+    with pytest.raises(ValueError):
+        zero_q = Quaternion(0, 0, 0, 0)
+        zero_q.normalize()
+
+
+def test_quaternion_inverse():
+    q1 = Quaternion(1, 2, 3, 4)
+    inv = q1.inverse()
+    assert q1 * inv == Quaternion.identity()
+
+    with pytest.raises(ValueError):
+        zero_q = Quaternion(0, 0, 0, 0)
+        zero_q.inverse()
